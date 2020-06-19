98597e3a2f720b9573606162b0fdddb8a331485b
haydenhitt1094 <haydenhitt10@gmail.com>
2020-06-19 16:15:01 -0500

    Stats Updated

--- Statistician.py
+++ Statistician.py
@@ -5,16 +5,34 @@ class StatsKeeper:
         self.shareprice = shareprice
         self.amount = amount
         self.costbasis = self.amount * self.shareprice
+        self.isWriting = False
        
     def submit(self):
         import json
+        self.isWriting = True
         originalContent = []
         try:
-            for item in json.load(open("Holdings.json","r")):
-                originalContent.append(item)
+            for entry in json.load(open("Transactions.json","r")):
+                originalContent.append(entry)
         except:
             pass
         originalContent.append(dict({str(self.symbol):{"Shareprice":self.shareprice,"Amount":self.amount,"Costbasis":self.costbasis}}))
-        json.dump(originalContent,open("Holdings.json",'w'),indent=4)
-        return str("Done")
+        json.dump(originalContent,open("Transactions.json",'w'),indent=4)
+        self.isWriting = False
+        return str("Transactions list updated")
+
+    def retrieve_all(self):
+        foundResults = []
+        transactions = json.load(open("Transactions.json",'r'))
+        
+        for jsonIndex in range(len(transactions)):
+            try:
+                entry = transactions[jsonIndex][str(self.symbol)]
+                foundResults.append(entry)
+            except:
+                pass
+        return foundResults
+    
+    def updateHistory(self):
+        with open()
 
